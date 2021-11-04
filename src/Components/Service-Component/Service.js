791c924b3f7ee0f1cd81ff5a3c4a8928ef08e5f6
Nadim18-lab <nadincho18@gmail.com>
2021-11-04 16:23:24 -0300

    fondo y modificacion de cartas

--- src/Components/Service-Component/Service.js
+++ src/Components/Service-Component/Service.js
@@ -6,36 +6,62 @@ class Service extends Component {
   render() {
     return (
       <div>
-        <h1 class="Titulo">Sobre Nosotros</h1>
-        <div class="cardPadre">
-        <div class="card">
-          <img src="..." class="card-img-top" alt="..."></img>
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
-            </a>
+        <div><img class="fondo" src="https://www.mediasource.mx/hubfs/blog-files/fondos-web.jpg"></img></div>
+          <h1 class="Titulo">Sobre Nosotros</h1>
+          <div class="cardPadre">
+            <div class="card">
+              <img
+                src="https://i.ytimg.com/vi/9FlBtZiMCAw/mqdefault.jpg"
+                class="card-img-top"
+                alt="..."
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
             </div>
-          </div>
-          <div class="card">
-          <img src="..." class="card-img-top" alt="..."></img>
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">
-              Go somewheree
-            </a>
+            <div class="card">
+              <img
+                src="https://i.ytimg.com/vi/9FlBtZiMCAw/mqdefault.jpg"
+                class="card-img-top"
+                alt="..."
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewheree
+                </a>
+              </div>
             </div>
-          </div>
+            <div class="card">
+              <img
+                src="https://i.ytimg.com/vi/9FlBtZiMCAw/mqdefault.jpg"
+                class="card-img-top"
+                alt="..."
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewheree
+                </a>
+              </div>
+            </div>
+            </div>
         </div>
-      </div>
     );
   }
 }

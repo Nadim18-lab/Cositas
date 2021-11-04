c89148155199b811da001706fa801d70164d8789
dalmiroflores <dalmiro2h@gmail.com>
2021-11-04 16:26:05 -0300

    progreso con maquetado de contact page

--- src/Components/Contact-Component/Contact.js
+++ src/Components/Contact-Component/Contact.js
@@ -8,68 +8,82 @@ class Contact extends Component {
       <div>
         <div class="header">
           <h1>Contactanos</h1>
+          <p>
+            Amet consequat occaecat aliqua veniam adipisicing quis dolor in amet
+            eiusmod.
+            <br /> Consectetur anim consectetur Lorem sunt culpa dolore est amet
+            ea deserunt dolor enim.
+            <br /> Proident ullamco minim elit amet deserunt laborum ullamco
+            aute nulla.
+          </p>
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Deja tus datos</h5>
               <form>
-                <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">
-                    Nombre
-                  </label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  ></input>
+                <div class="float-left">
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1  " class="form-label">
+                      Nombre
+                    </label>
+                    <input
+                      type="text"
+                      class="input-style form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                    ></input>
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1  " class="form-label">
+                      Apellido
+                    </label>
+                    <input
+                      type="text"
+                      class="input-style form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                    ></input>
+                  </div>
                 </div>
-                <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">
-                    Apellido
-                  </label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  ></input>
+                <div class="float-right">
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1  " class="form-label">
+                      Numero
+                    </label>
+                    <input
+                      type="text"
+                      class="input-style form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                    ></input>
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1  " class="form-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      class="input-style form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                    ></input>
+                  </div>
                 </div>
-                <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">
-                    Numero
-                  </label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  ></input>
+                <div class="question-container">
+                  <div>
+                    <label for="exampleInputEmail1  " class="form-label">
+                      Consulta
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control question-input"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                    ></input>
+                  </div>
+                  <button type="submit" class="submit-btn btn btn-primary">
+                    Enviar
+                  </button>
                 </div>
-                <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  ></input>
-                </div>
-                <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">
-                    Consulta
-                  </label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  ></input>
-                </div>
-                <button type="submit" class="btn btn-primary">
-                  Submit
-                </button>
               </form>
             </div>
           </div>

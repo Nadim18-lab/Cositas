2332fae406b016a5f6c860b18ce0a5d6658d64e4
dalmiroflores <dalmiro2h@gmail.com>
2021-11-08 15:30:54 -0300

    Enrutamiento de las seccines del navbar

--- src/Components/NavBar-Component/NavBar.js
+++ src/Components/NavBar-Component/NavBar.js
@@ -1,40 +1,44 @@
 import React, { Component } from "react";
 import { PropTypes } from "prop-types";
-import './NavBar.css';
+import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
-
-    render() {
-        return (
-            <div>
-                <div>
-                    <ul className = "nav">
-                        <div className = "nav_div">
-                            <li><a href = "/#">Home</a></li>
-                        </div>
-                    </ul>
-                    <ul className = "nav">
-                        <div className = "nav_div">
-                            <li><a href = "/#">About us</a></li>
-                        </div>
-                    </ul>
-                    <ul className = "nav">
-                        <div className = "nav_div">
-                            <li><a href = "/#">Contact us</a></li>
-                        </div>
-                    </ul>
-                </div>
-                <div className = "header">
-                    <div className = "header_title">
-                        <h1>TITLE</h1>
-                    </div>
-                    <div>
-
-                    </div>   
-                </div>
+  render() {
+    return (
+      <div>
+        <div>
+          <ul className="nav">
+            <div className="nav_div">
+              <li>
+                <Link to={"/home"}>Home</Link>
+              </li>
             </div>
-        );
-    }
+          </ul>
+          <ul className="nav">
+            <div className="nav_div">
+              <li>
+                <Link to={"/contact"}>Contacto</Link>
+              </li>
+            </div>
+          </ul>
+          <ul className="nav">
+            <div className="nav_div">
+              <li>
+                <Link to={"/service"}>Servicio</Link>
+              </li>
+            </div>
+          </ul>
+        </div>
+        <div className="header">
+          <div className="header_title">
+            <h1>TITLE</h1>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NavBar;

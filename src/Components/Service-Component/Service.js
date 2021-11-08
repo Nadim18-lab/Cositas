ad6f1b8b0b8ca2e23c6f72726638b3b938b01fc1
Nadim18-lab <nadincho18@gmail.com>
2021-11-08 16:47:12 -0300

    cambio

--- src/Components/Service-Component/Service.js
+++ src/Components/Service-Component/Service.js
@@ -11,42 +11,42 @@ class Service extends Component {
             <h1 class="Titulo">Sobre Nosotros</h1>
             <div class="cardPadre">
               <div class="card">
-                <div class="icon-container"><i class="fas fa-images icon"></i></div>
+                <div class="icon-container">
+                  <i class="fas fa-images icon"></i>
+                </div>
                 <div class="card-body">
                   <h5 class="card-title">Card title</h5>
                   <p class="card-text">
                     Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    up the bulk of the card's content.Some quick example text to
+                    build on the card title and make up the bulk of the card's
+                    content.Some quick example text to build on the card title
+                    and make
                   </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
-                  </a>
                 </div>
               </div>
               <div class="card">
-                <div class="icon-container"><i class="fab fa-apple icon2"></i></div>
+                <div class="icon-container">
+                  <i class="fab fa-apple icon2"></i>
+                </div>
                 <div class="card-body">
                   <h5 class="card-title">Card title</h5>
                   <p class="card-text">
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
                   </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
-                  </a>
                 </div>
               </div>
               <div class="card">
-                <div class="icon-container"><i class="fas fa-dna icon3"></i></div>
+                <div class="icon-container">
+                  <i class="fas fa-dna icon3"></i>
+                </div>
                 <div class="card-body">
                   <h5 class="card-title">Card title</h5>
                   <p class="card-text">
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
                   </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
-                  </a>
                 </div>
               </div>
             </div>

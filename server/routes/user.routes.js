8b37a553d6b27eddb3c8b6eb53ba7ed61a5a55d4
dalmiroflores <dalmiro2h@gmail.com>
2021-11-17 16:05:52 -0300

    Realizado y terminado de API

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const user = require("../models/user");
 const router = express.Router();
-const User = require('../models/user')
-router.get("/users", (req, res) => {
+const User = require("../models/user");
 
-  user.find((err, users)=>{
-    console.log(users)
-  })
+router.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+router.post("/users", async (req, res) => {
+  const { name, lastName, email, phoneNumber, question } = req.body;
+  const user = new User({ name, lastName, email, phoneNumber, question });
+  await user.save();
+  res.json({ status: "User saved!" });
+});
+router.put("/users/:id", async (req, res) => {
+  const { name, lastName, email, phoneNumber, question } = req.body;
+  const newUser = { name, lastName, email, phoneNumber, question };
+  await User.findByIdAndUpdate(req.params.id, newUser);
+  res.json({ status: "User Updated!!" });
+});
+
+router.delete("/users/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ status: "User Deleted!!" });
 });
 
 module.exports = router;
